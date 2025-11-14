50a203edc76a44f1e147b97dc0fa5f418fb3737c
JesusManuel1102 <167624650+JesusManuel1102@users.noreply.github.com>
2025-11-14 11:00:31 -0500

    Delete src directory

--- src/config/database.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "../generated/prisma";
-const prisma = new PrismaClient();
-
-prisma.$connect().catch((err) => {
-  console.error("Prisma connection error:", err);
-  process.exit(1);
-});
-
-export default prisma;
--- src/middleware/showRequest.ts
+++
@@ -1,8 +0,0 @@
-import { RequestHandler } from 'express'
-
-const showRequests: RequestHandler = async (req, _res, next) => {
-  console.log(` ${req.path} - ${req.method} `)
-  next()
-}
-
-export default showRequests
